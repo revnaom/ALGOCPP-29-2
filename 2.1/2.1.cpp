bd015cc4dbb215f647c7c8c6646771980362c6b2
Louise Omarova <ruizu.om21@gmail.com>
2025-04-24 14:02:15 +0300

    first commit

---
+++ 2.1/2.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int fibonacci(int n) 
+{
+    if (n <= 1) 
+    {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() 
+{
+    int n;
+    std::cout << "Введите число n: ";
+    std::cin >> n;
+    std::cout << "Число Фибоначчи F(" << n << ") = " << fibonacci(n) << std::endl;
+    return 0;
+}
